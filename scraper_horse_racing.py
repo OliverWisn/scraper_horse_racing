15acd156286a5d42af51bb781f039e3b12e8c2a7
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-03-07 16:36:56 +0100

    Organize the code
    Organize the code.

--- scraper_horse_racing.py
+++ scraper_horse_racing.py
@@ -10,7 +10,8 @@ import requests
 from bs4 import BeautifulSoup
 
 # Variable with the URL of the website.
-my_url = "https://www.whatismybrowser.com/detect/what-http-headers-is-my-browser-sending"
+my_url = "https://www.whatismybrowser.com/detect"+\
+"/what-http-headers-is-my-browser-sending"
 
 # Preparing of the Tor browser for the work.
 torexe = os.popen(\
@@ -26,16 +27,16 @@ profile.update_preferences()
 firefox_options = Options()
 driver = Firefox(firefox_profile= profile, options = firefox_options)
 
+# Adding the headers to the browser
 session = requests.Session()
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"}
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0)"+\
+" Gecko/20100101 Firefox/97.0", "Accept": "text/html,application"+\
+"/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"}
 req = session.get(my_url, headers=headers)
 # bsObj = BeautifulSoup(req.text, "lxml")
 # print(bsObj.find("table", {"class": "table-striped"}).get_text)
 
 driver.get(my_url)
-
-
-
 
 
 # driver.close()

77ab45cd40748afdf5f940e91830861310d84dd6
Olgierd Wisniewski <79323713+OliverWisn@users.noreply.github.com>
2022-06-19 13:50:48 +0200

    Add the exception handling
    The handling of the exceptions has been added.

--- scraper_horse_racing.py
+++ scraper_horse_racing.py
@@ -10,6 +10,7 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -57,7 +58,7 @@ def _addingheaders(my_url):
 
 def scrapingitems(driver, hippodromes, my_list, end_xpath):
         """
-        Create appropriate lists from the data for the pandas library.
+        Create appropriate lists of the data for the pandas library.
         """
         for ind in range(1, (len(hippodromes)+1)):
             my_list.append('')
@@ -111,7 +112,11 @@ try:
     element = WebDriverWait(driver, 120).until(
         EC.presence_of_element_located((By.CLASS_NAME,
         'boxOverContent__bannerLink')))
-finally:
+except TimeoutException:
+    print("Loading took too much time!. Please rerun the script.")
+except Exception as e:
+    print(str(e))
+else:
     # Loads the website code as the BeautifulSoup object.
     pageSource = driver.page_source
     bsObj = BeautifulSoup(pageSource, 'lxml')
@@ -516,5 +521,5 @@ finally:
         "%Y%m%d-%H.%M.%S"))
     df_res.to_csv(name_of_file(), encoding="utf-8")
 
-
+finally:
     driver.quit()
